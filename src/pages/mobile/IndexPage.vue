de7349d05888c5d391792203098506b846f5c15c
Jovi <jovi.anto@misteraladin.com>
2022-05-04 09:25:51 +0700

    [Page] update mobile page.
    - [Composable] use movie popular.

--- src/pages/mobile/IndexPage.vue
+++ src/pages/mobile/IndexPage.vue
@@ -1,11 +1,95 @@
 <script lang="ts">
-import { defineComponent } from 'vue';
+import useMoviePopular from 'src/composables/useMoviePopular';
+import { date } from 'quasar';
+import { defineComponent, onMounted, ref } from 'vue';
 
 export default defineComponent({
   name: 'MobileIndexPage',
+  setup() {
+    const moviePopular = useMoviePopular();
+
+    const moreDialog = ref(false);
+
+    const loadMore = async (entry: IntersectionObserverEntry) => {
+      if (moviePopular.$state.page >= moviePopular.$state.lastPage) {
+        return;
+      }
+      if (entry.isIntersecting) {
+        await moviePopular.loadMore();
+      }
+    };
+
+    onMounted(() => {
+      moviePopular.get();
+    });
+
+    return {
+      date,
+      moviePopular,
+      moreDialog,
+      loadMore,
+    };
+  },
 });
 </script>
 
 <template>
-  <q-page class="items-center justify-evenly row">Mobile page</q-page>
+  <q-page>
+    <div>
+      <q-card
+        class="q-mb-md"
+        flat
+        square
+        v-for="movie in moviePopular.$state.data"
+        :key="movie.id"
+      >
+        <q-img
+          fit="contain"
+          :alt="movie.title"
+          :ratio="16 / 9"
+          :src="movie.poster"
+        />
+        <q-list>
+          <q-item>
+            <q-item-section>
+              <q-item-label>{{ movie.title }}</q-item-label>
+              <q-item-label caption>
+                {{ movie.totalView }} views •
+                {{
+                  date.formatDate(Date.parse(movie.releaseDate), 'MMM DD, YYYY')
+                }}
+              </q-item-label>
+            </q-item-section>
+            <q-item-section side top>
+              <q-btn
+                dense
+                flat
+                icon="more_vert"
+                round
+                @click="moreDialog = true"
+              />
+            </q-item-section>
+          </q-item>
+        </q-list>
+      </q-card>
+    </div>
+    <div v-intersection="loadMore"></div>
+    <div class="text-center" v-if="moviePopular.$state.loadMoreLoading">
+      <q-spinner size="md" />
+    </div>
+    <q-dialog full-width v-model="moreDialog">
+      <q-card square>
+        <q-item clickable v-ripple>
+          <q-item-section>
+            <q-item-label>Save to Watch later</q-item-label>
+          </q-item-section>
+        </q-item>
+        <q-item clickable v-ripple @click="moreDialog = false">
+          <q-item-section>
+            <q-item-label>Cancel</q-item-label>
+          </q-item-section>
+        </q-item>
+      </q-card>
+    </q-dialog>
+  </q-page>
 </template>
