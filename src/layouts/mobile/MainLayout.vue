cb9116972f319014e748741b3d481f973538a6ab
Jovi <jovi.anto@misteraladin.com>
2022-05-04 07:08:28 +0700

    [Layout] update mobile main layout.

--- src/layouts/mobile/MainLayout.vue
+++ src/layouts/mobile/MainLayout.vue
@@ -1,13 +1,114 @@
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, ref } from 'vue';
 
 export default defineComponent({
   name: 'MobileMainLayout',
+  setup() {
+    const search = ref('');
+    const searchDialog = ref(false);
+    const accountDialog = ref(false);
+
+    return {
+      search,
+      searchDialog,
+      accountDialog,
+    };
+  },
 });
 </script>
 
 <template>
-  <q-layout>
+  <q-layout view="lHh lpr lFf">
+    <q-header bordered class="bg-white text-black" reveal>
+      <q-toolbar>
+        <q-btn flat label="YouTube" :to="{ name: 'mobile' }" />
+        <q-space />
+        <q-btn flat icon="search" round @click="searchDialog = true" />
+        <q-dialog full-width position="top" v-model="searchDialog">
+          <q-card>
+            <q-toolbar>
+              <q-btn
+                flat
+                icon="arrow_back"
+                round
+                @click="searchDialog = false"
+              />
+              <q-input
+                class="col"
+                dense
+                placeholder="Search YouTube"
+                v-model="search"
+              >
+                <template v-slot:after>
+                  <q-btn
+                    flat
+                    icon="close"
+                    round
+                    v-if="search"
+                    @click="search = ''"
+                  />
+                  <q-btn flat icon="search" round />
+                  <q-btn flat icon="mic" round v-if="!search" />
+                </template>
+              </q-input>
+            </q-toolbar>
+          </q-card>
+        </q-dialog>
+        <q-btn
+          flat
+          icon="o_account_circle"
+          round
+          @click="accountDialog = true"
+        />
+        <q-dialog maximized v-model="accountDialog">
+          <q-card>
+            <q-item>
+              <q-item-section avatar>
+                <q-icon name="o_close" @click="accountDialog = false" />
+              </q-item-section>
+              <q-item-section>Account</q-item-section>
+            </q-item>
+            <q-separator />
+            <q-separator />
+            <q-separator />
+            <q-item clickable v-ripple>
+              <q-item-section avatar>
+                <q-icon name="o_switch_account" />
+              </q-item-section>
+              <q-item-section>Sign In</q-item-section>
+            </q-item>
+            <q-separator />
+            <q-item clickable v-ripple>
+              <q-item-section avatar>
+                <q-icon name="o_settings" />
+              </q-item-section>
+              <q-item-section>Settings</q-item-section>
+            </q-item>
+            <q-separator />
+            <q-item clickable v-ripple>
+              <q-item-section avatar>
+                <q-icon name="o_help_outline" />
+              </q-item-section>
+              <q-item-section>Help</q-item-section>
+            </q-item>
+            <q-item clickable v-ripple>
+              <q-item-section avatar>
+                <q-icon name="o_feedback" />
+              </q-item-section>
+              <q-item-section>Feedback</q-item-section>
+            </q-item>
+            <q-separator />
+          </q-card>
+        </q-dialog>
+      </q-toolbar>
+    </q-header>
+    <q-footer bordered class="bg-white text-black">
+      <q-tabs dense no-caps>
+        <q-tab icon="house" label="Home" name="home" />
+        <q-tab icon="o_slideshow" label="Shorts" name="shorts" />
+        <q-tab icon="o_video_library" label="Library" name="library" />
+      </q-tabs>
+    </q-footer>
     <q-page-container>
       <router-view />
     </q-page-container>
